d8ea85f56c77208a6d6f0666fa260d67e2d10eae
Enzo <enzobulacio18@gmail.com>
2020-05-26 15:14:01 -0300

    punto1

---
+++ TP6/TP6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TP6
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int num, resto;
+                int invertido = 0;
+                Console.WriteLine("ingrese mayor a 0 un numero");
+                num = int.Parse(Console.ReadLine());
+                if (num > 0)
+                {
+                    while (num > 0)
+                    {
+
+                        resto = num % 10;
+                        invertido = invertido * 10 + resto;
+                        num = num / 10;
+
+                    }
+                    Console.WriteLine(invertido);
+
+                }
+                else
+                    Console.WriteLine("no es un numero mayor a 0");
+            }
+        }
+    }
